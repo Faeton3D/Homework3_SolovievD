a23b6299b12d17afd49ba7f9036275addc556878
Faeton3D <plus0503@gmail.com>
2022-04-28 14:20:58 +0300

    Задание 2

---
+++ Homework2/Program.cs
@@ -0,0 +1,10 @@
+﻿double xa = 3;
+double ya = 6;
+double za = 8;
+double xb = 2;
+double yb = 1;
+double zb = -7;
+
+double S = (xa-xb)*(xa-xb)+(ya-yb)*(ya-yb)+(za-zb)*(za-zb);
+S = Math.Sqrt(S);
+Console.WriteLine(S);
